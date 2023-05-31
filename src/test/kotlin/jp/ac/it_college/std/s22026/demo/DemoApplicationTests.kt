00b8fd4b5beddb93e368211e8e86900d00560022
s22026 <s22026@std.it-college.ac.jp>
2023-05-31 12:06:29 +0900

    Spring Boot initial commit

---
+++ src/test/kotlin/jp/ac/it_college/std/s22026/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s22026.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
